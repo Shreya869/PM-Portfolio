211c5b98a03f049c69d3da9ee2ea531b84347f7e
Shreya <62697529+Shreya869@users.noreply.github.com>
2021-05-15 19:59:37 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Shortfolio
+ ![GitHub](https://img.shields.io/github/license/imfunniee/fimbo.svg?style=popout-square) 
+
+![Image of the website](https://github.com/Aishanipach/shortfolio/blob/master/img/readme.jpg)
+
+Template for you to maintain your projects in a <b>short portfolio</b> with a <b>blog display</b> section .<br><br>
+
+## Check it out:
+[Click here!](https://aishanipach.github.io/shortfolio/)
+
+## How To Use?
+
+-  ### Click on Use this Template
+![Image](https://github.com/Aishanipach/shortfolio/blob/master/Usethis.PNG)
+  
+  
+ -  ### Customize & Contribute!
+     - Fork the project & clone locally.
+     - Create an upstream remote and sync your local copy before you branch.
+     - Branch for each separate piece of work.
+     - Create a new Pull Request in GitHub.
---
+++ index.js
@@ -0,0 +1,28 @@
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+$(document).ready(function() {
+/* About me slider */
+$('.about-me-slider').slick({
+  slidesToShow: 1,
+  prevArrow: '<span class="span-arrow slick-prev"><</span>',
+  nextArrow: '<span class="span-arrow slick-next">></span>'
+});
+
+});
